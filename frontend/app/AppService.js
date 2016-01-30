5e7118ebd05a76986949b20b449cd702c66ff03e
Zach Waterfield <zlwaterf@uwaterloo.ca>
2016-01-30 00:43:32 -0500

    Sort widgets

--- frontend/app/AppService.js
+++ frontend/app/AppService.js
@@ -62,7 +62,8 @@
         row: 0,
         col: 0,
         text: 'begin',
-        editable: false
+        editable: false,
+        removeable: false
       }
     ];
 
@@ -74,40 +75,6 @@
       { text: 'end loop', moduleNum: 1}
     ];
 
-    var gridsterOpts = {
-        columns: 12, // the width of the grid, in columns
-        pushing: false, // whether to push other items out of the way on move or resize
-        floating: false, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
-        swapping: true, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
-        width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
-        colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
-        rowHeight: 80, // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
-        margins: [10, 10], // the pixel distance between each widget
-        outerMargin: true, // whether margins apply to outer edges of the grid
-        isMobile: false, // stacks the grid items if true
-        mobileBreakPoint: 200, // if the screen is not wider that this, remove the grid layout and stack the items
-        mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
-        minColumns: 1, // the minimum columns the grid must have
-        minRows: 2, // the minimum height of the grid, in rows
-        maxRows: 20,
-        defaultSizeX: 4, // the default width of a gridster item, if not specifed
-        defaultSizeY: 2, // the default height of a gridster item, if not specified
-        minSizeX: 1, // minimum column width of an item
-        maxSizeX: null, // maximum column width of an item
-        minSizeY: 1, // minumum row height of an item
-        maxSizeY: null, // maximum row height of an item
-        resizable: {
-           enabled: false
-        },
-        draggable: {
-           enabled: true, // whether dragging items is supported
-           handle: '.my-class', // optional selector for resize handle
-           start: function(event, $element, widget) {}, // optional callback fired when drag is started,
-           drag: function(event, $element, widget) {}, // optional callback fired when item is moved,
-           stop: function(event, $element, widget) {} // optional callback fired when item is finished dragging
-        }
-    };
-
     // Promise-based API
     return {
       loadAllModules : function() {
@@ -115,8 +82,7 @@
         return $q.when(modules);
       },
       standardItems: standardItems,
-      moduleButtons: moduleButtons,
-      gridsterOpts: gridsterOpts
+      moduleButtons: moduleButtons
     };
   }
 
